a57d10b8e24f63f4cc378c1e650195cf7b361d7f
RecursionExcursion <rloup15@gmail.com>
2025-06-07 13:10:55 -0400

    types export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
 export { rank } from "./lib/ranker";
-export { RankerTeam, RankerGame, WeekTeamsMap } from "./lib/ranker-types";
+export { RankerTeam, RankerGame, WeekTeamsMap, RankerStat, RankerGameStats, } from "./lib/ranker-types";
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { rank } from "./lib/ranker";
-export { RankerTeam, RankerGame, WeekTeamsMap } from "./lib/ranker-types";
+export {
+  RankerTeam,
+  RankerGame,
+  WeekTeamsMap,
+  RankerStat,
+  RankerGameStats,
+} from "./lib/ranker-types";
