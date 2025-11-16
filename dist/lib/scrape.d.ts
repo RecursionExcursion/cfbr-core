1440b06fdec57a072fbe49f072f0e9f145908682
RecursionExcursion <rloup15@gmail.com>
2025-11-16 12:08:13 -0500

    rm scraper

--- dist/lib/scrape.d.ts
+++
@@ -1,8 +0,0 @@
-import { ESPNSeason } from "./scraper-types";
-export declare function scrape(year: number): Promise<void>;
-export declare function collectSeasonDates(startDate: Date, endDate: Date): void;
-export declare function getZeroDay(year: number): Promise<ESPNSeason | undefined>;
-export declare function getSeasonDateRanges(szn: ESPNSeason): {
-    startDate: Date;
-    endDate: Date;
-} | undefined;
